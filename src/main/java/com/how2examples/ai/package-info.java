916833dc60122b22b0c9206529aa936bb9517a1c
webber-s <webbers.github@gmail.com>
2014-03-08 11:59:09 +0000

    package-info

---
+++ src/main/java/com/how2examples/ai/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Implementations of common AI techniques.
+ * <p>
+ * This project has been developed for the sole purpose of improving my understanding of AI - 
+ * rather than as a comprehensive collection of efficient scalable techniques appropriate for use in mission critical systems.
+ */
+package com.how2examples.ai;
---
+++ src/main/java/com/how2examples/ai/problem/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Models of classic AI problems.
+ * <p>
+ * Used to develop a better understanding of the AI techniques contained in this project.
+ */
+package com.how2examples.ai.problem;
